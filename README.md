fcea0d75c707513f7130919e9237cf3a792870c1
Aaditya GPGPU <aprakash@brandeis.edu>
2016-04-03 14:58:25 -0400

    added argparser

---
+++ README.md
@@ -0,0 +1,51 @@
+#VQA Demo
+
+This is a simple Demo of Visual Question answering which uses pretrained models (see models/CNN and models/VQA) to answer a given question about the given image.
+
+## Dependency
+
+1. Keras
+   * Modular deep learning library based on python
+
+2. Theano/Tensorflow 
+    * As backend for Keras, you may choose either one. 
+    * For the development of this project, I used Theano 0.8.0
+
+3. scikit-learn
+   * Quintessential machine library for python
+
+4. Spacy 
+    * Used to load Glove vectors (word2vec)
+    * You may have to upgrade your Spacy to use Glove vectors (default is Goldberg Word2Vec)
+    * To upgrade & install Glove Vectors
+      * pip install --upgrade spacy
+      * sputnik —name spacy install en
+      * sputnik --name spacy install en_glove_cc_300_1m_vectors
+
+5. OpenCV 
+    * OpenCV is used only to resize the image and change the color channels,
+    * You may use other libraries as long as you can pass a 224x224 BGR Image (NOTE: BGR and not RGB)
+
+##Usage
+
+> python demo.py --file `path_to_file` --question "Question to be asked"
+
+e.g 
+
+> python demo.py --file test.jpg --question "Is there a man in the picture?"
+
+
+if you have prefer to use Theano backend and if you have GPU you may want to run like this
+
+> THEANO_FLAGS='floatX=float32,device=gpu0,lib.cnmem=1,mode=FAST_RUN' python demo.py -image_file_name test.jpg -question "What vechile is in the picture?"
+
+
+Expected Output :
+
+78.32 %  train
+01.11 %  truck
+00.98 %  passenger
+00.95 %  fire truck
+00.68 %  bus
+
+
--- demo.py
+++ demo.py
@@ -11,8 +11,9 @@ VQA_model_file_name     = 'models/VQA/VQA_MODEL.json'
 VQA_weights_file_name   = 'models/VQA/VQA_MODEL_WEIGHTS.hdf5'
 label_encoder_file_name = 'models/VQA/FULL_labelencoder_trainval.pkl'
 CNN_weights_file_name   = 'models/CNN/vgg16_weights.h5'
-image_file_name         = 'test.jpg'
-question                = unicode("Where is the man?", "utf-8")
+
+# Chagne the value of verbose to 0 to avoid printing the progress statements
+verbose = 1
 
 def get_image_model(CNN_weights_file_name):
     image_model = VGG_16(CNN_weights_file_name)
@@ -36,25 +37,33 @@ def get_VQA_model(VQA_model_file_name, VQA_weights_file_name):
 
 def get_question_features(question):
     word_embeddings = spacy.load('en', vectors='en_glove_cc_300_1m_vectors')
-    timesteps = len(word_embeddings(question)) #question sorted in descending order of length
-    nb_samples = 1
-    word_vec_dim = 300
-    question_tensor = np.zeros((nb_samples, timesteps, word_vec_dim))
     tokens = word_embeddings(question)
+    question_tensor = np.zeros((1, len(tokens), 300))
     for j in xrange(len(tokens)):
-        if j<timesteps:
-                question_tensor[0,j,:] = tokens[j].vector
+        # if j<timesteps:
+            question_tensor[0,j,:] = tokens[j].vector
     return question_tensor
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-image_file_name', type=str, default='test.jpg')
+    parser.add_argument('-question', type=str, default='What vechile is in the picture?')
+    args = parser.parse_args()
+
+    
+    if verbose : print("\n\n\nLoading image features ...")
+    image_features = get_image_features(args.image_file_name, CNN_weights_file_name)
+
+    if verbose : print("Loading question features ...")
+    question_features = get_question_features(unicode(args.question, 'utf-8'))
+
+    if verbose : print("Loading VQA Model ...")
     vqa_model = get_VQA_model(VQA_model_file_name, VQA_weights_file_name)
-    image_features = get_image_features(image_file_name, CNN_weights_file_name)
-    question_features = get_question_features(question)
 
-    X = [question_features, image_features]
 
-    y_output = vqa_model.predict(X)
+    if verbose : print("\n\n\nPredicting result ...") 
+    y_output = vqa_model.predict([question_features, image_features])
     y_sort_index = np.argsort(y_output)
 
     labelencoder = joblib.load(label_encoder_file_name)
